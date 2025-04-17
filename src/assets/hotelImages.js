71e04cdf93f9ca5526cbea7c18591d180cdbe302
Haider Mukhtar <haidermukhtarch405@gmail.com>
2025-04-17 21:37:15 +0500

    images add and imported

---
+++ src/assets/hotelImages.js
@@ -0,0 +1,156 @@
+// Monsieur Georges Hotel Images
+import MonsieurGeorges1 from './Monsieur-Georges/Hotel-Monsieur-Georges.jpg';
+import MonsieurGeorges2 from './Monsieur-Georges/bar-hotel-monsieur-georges.jpg';
+import MonsieurGeorges3 from './Monsieur-Georges/bar-hotel-monsieur-georges-2.jpg';
+import MonsieurGeorges4 from './Monsieur-Georges/bar-hotel-monsieur-georges-3.jpg';
+import MonsieurGeorges5 from './Monsieur-Georges/bar-hotel-monsieur-georges-4.jpg';
+import MonsieurGeorges6 from './Monsieur-Georges/bar-hotel-monsieur-georges-5.jpg';
+import MonsieurGeorges7 from './Monsieur-Georges/chambre-hotel-proche-avenue-kleber.jpg';
+import MonsieurGeorges8 from './Monsieur-Georges/chambre-hotel-proche-avenue-kleber-2.jpg';
+import MonsieurGeorges9 from './Monsieur-Georges/hotel-proche-avenue-kleber.jpg';
+import MonsieurGeorges10 from './Monsieur-Georges/hotel-proche-avenue-kleber-2.jpg';
+import MonsieurGeorges11 from './Monsieur-Georges/room-near-avenue-kleber.jpg';
+import MonsieurGeorges12 from './Monsieur-Georges/room-near-avenue-kleber-2.jpg';
+import MonsieurGeorges13 from './Monsieur-Georges/room-near-avenue-kleber-3.jpg';
+
+// La Demeure Montaigne Hotel Images
+import LaDemeureMontaigne1 from './La-Demeure-Montaigne/Boutique-Hotel-Paris-avenue-Kleber.jpg';
+import LaDemeureMontaigne2 from './La-Demeure-Montaigne/Boutique-Hotel-Paris-Kleber';
+import LaDemeureMontaigne3 from './La-Demeure-Montaigne/Hotel-Boutique-Paris-avenue-Kleber.jpg';
+import LaDemeureMontaigne4 from './La-Demeure-Montaigne/Hotel-Kleber-Quarter-Paris.jpg';
+import LaDemeureMontaigne5 from './La-Demeure-Montaigne/Hotel-Kleber-Quarter-Paris-2.jpg';
+import LaDemeureMontaigne6 from './La-Demeure-Montaigne/Hotel-Kleber-Quarter-Paris-3.jpg';
+import LaDemeureMontaigne7 from './La-Demeure-Montaigne/Hotel-Kleber-Quarter-Paris-4.jpg';
+import LaDemeureMontaigne8 from './La-Demeure-Montaigne/Hotel-Kleber-Quarter-Paris-luxury.jpg';
+import LaDemeureMontaigne9 from './La-Demeure-Montaigne/Hotel-Luxe-Paris-Kleber.jpg';
+import LaDemeureMontaigne10 from './La-Demeure-Montaigne/Hotel-near-avenue-Kleber-Paris.jpg';
+import LaDemeureMontaigne11 from './La-Demeure-Montaigne/Hotel-near-avenue-Kleber-Paris-2.jpg';
+import LaDemeureMontaigne12 from './La-Demeure-Montaigne/Hotel-near-Kleber-Paris.jpg';
+import LaDemeureMontaigne13 from './La-Demeure-Montaigne/Hotel-near-Kleber-Paris-2.jpg';
+import LaDemeureMontaigne14 from './La-Demeure-Montaigne/Hotel-near-Kleber-Paris-3.jpg';
+import LaDemeureMontaigne15 from './La-Demeure-Montaigne/Hotel-proche-kleber-paris.jpg';
+import LaDemeureMontaigne16 from './La-Demeure-Montaigne/Hotel-proche-kleber-paris-2.jpg';
+import LaDemeureMontaigne17 from './La-Demeure-Montaigne/Hotel-proche-kleber-paris-3.jpg';
+import LaDemeureMontaigne18 from './La-Demeure-Montaigne/Hotel-proche-Kleber-Paris-4.jpg';
+import LaDemeureMontaigne19 from './La-Demeure-Montaigne/Hotel-Quartier-Kleber-Paris.jpg';
+import LaDemeureMontaigne20 from './La-Demeure-Montaigne/Hotel-Quartier-Kleber-Paris-2.jpg';
+import LaDemeureMontaigne21 from './La-Demeure-Montaigne/Luxury-paris-boutique-hotel-near-kleber.jpg';
+import LaDemeureMontaigne22 from './La-Demeure-Montaigne/Paris-Hotel-Kleber.jpg';
+import LaDemeureMontaigne23 from './La-Demeure-Montaigne/Paris-Hotel-Kleber-Quarter.jpg';
+import LaDemeureMontaigne24 from './La-Demeure-Montaigne/Paris-Hotel-Kleber-Quarter-2.jpg';
+import LaDemeureMontaigne25 from './La-Demeure-Montaigne/Paris-Hotel-Kleber-Quarter-3.jpg';
+import LaDemeureMontaigne26 from './La-Demeure-Montaigne/room-Hotel-Paris-Kleber.jpg';
+
+// Hotel Pastel Images
+import HotelPastel1 from './Hotel-Pastel/Boutique-hotel-Paris-hotel-Pastel-Kleber.jpg';
+import HotelPastel2 from './Hotel-Pastel/Hotel-avenue-Kleber-Paris.jpg';
+import HotelPastel3 from './Hotel-Pastel/Hotel-Paris-proche-kleber.jpg';
+import HotelPastel4 from './Hotel-Pastel/Hotel-Pastel-chambre.jpg';
+import HotelPastel5 from './Hotel-Pastel/Hotel-Pastel-Kleber-breaksfast-rroom.jpg';
+import HotelPastel6 from './Hotel-Pastel/Hotel-Pastel-near-avenue-kleber.jpg';
+import HotelPastel7 from './Hotel-Pastel/Hotel-Pastel-proche-Kleber.jpg';
+import HotelPastel8 from './Hotel-Pastel/Hotel-proche-avenue-Kleber-Paris-Pastel.jpg';
+import HotelPastel9 from './Hotel-Pastel/Paris-hotel-near-Kleber.jpg';
+import HotelPastel10 from './Hotel-Pastel/Pastel-Boutique-hotel-Kleber-Paris.jpg';
+import HotelPastel11 from './Hotel-Pastel/Pastel-Hotel-room.jpg';
+
+// Hotel Padam Images
+import HotelPadam1 from './Hotel-Padam/Hotal-padam-paris-balcony.jpg';
+import HotelPadam2 from './Hotel-Padam/hotel-padam-Paris-bathroom.jpg';
+import HotelPadam3 from './Hotel-Padam/Hotel-padam-paris-chambre.jpg';
+import HotelPadam4 from './Hotel-Padam/Hotel-Padam-paris-near-kleber.jpg';
+import HotelPadam5 from './Hotel-Padam/Hotel-padam-paris-proche-kleber.jpg';
+import HotelPadam6 from './Hotel-Padam/Hotel-padam-proche-avenue-kleber.jpg';
+import HotelPadam7 from './Hotel-Padam/Padam-hotel-Paris-room.jpg';
+import HotelPadam8 from './Hotel-Padam/room-hotel-padam-paris.jpg';
+
+// Hotel Keppler Images
+import HotelKeppler1 from './Hotel-Keppler/boutique-hotel-keppler-paris.jpg';
+import HotelKeppler2 from './Hotel-Keppler/chambre-hotel-keppler.jpg';
+import HotelKeppler3 from './Hotel-Keppler/chambre-hotel-keppler-paris.jpg';
+import HotelKeppler4 from './Hotel-Keppler/chambre-hotel-keppler-proche-avenue-kleber-paris.jpg';
+import HotelKeppler5 from './Hotel-Keppler/chambre-hotel-proche-avenue-kleber-paris.jpg';
+import HotelKeppler6 from './Hotel-Keppler/hotel-keppler-bathroom.jpg';
+import HotelKeppler7 from './Hotel-Keppler/hotel-keppler-proche-avenue-kleber-paris.jpg';
+import HotelKeppler8 from './Hotel-Keppler/hotel-near-arc-the-triomphe-paris.jpg';
+import HotelKeppler9 from './Hotel-Keppler/hotel-near-avenue-kleber-paris.jpg';
+import HotelKeppler10 from './Hotel-Keppler/hotel-near-avenue-kleber-paris-5.jpg';
+import HotelKeppler11 from './Hotel-Keppler/hotel-near-chaps-elysees-paris.jpg';
+import HotelKeppler12 from './Hotel-Keppler/hotel-proche-avenue-kleber-paris.jpg';
+import HotelKeppler13 from './Hotel-Keppler/keppler hotel-near-avenue-kleber-paris.jpg';
+import HotelKeppler14 from './Hotel-Keppler/keppler-hotel-paris.jpg';
+import HotelKeppler15 from './Hotel-Keppler/keppler-hotel-paris-room.jpg';
+import HotelKeppler16 from './Hotel-Keppler/room-hotel-near-avenue-kleber-paris.jpg';
+import HotelKeppler17 from './Hotel-Keppler/room-hotel-near-avenue-kleber-paris-2.jpg';
+import HotelKeppler18 from './Hotel-Keppler/room-hotel-near-avenue-kleber-paris-3.jpg';
+import HotelKeppler19 from './Hotel-Keppler/room-hotel-near-avenue-kleber-paris-4.jpg';
+import HotelKeppler20 from './Hotel-Keppler/room-hotel-near-avenue-kleber-paris-5.jpg';
+import HotelKeppler21 from './Hotel-Keppler/room-hotel-near-avenue-kleber-paris-6.jpg';
+import HotelKeppler22 from './Hotel-Keppler/room-hotel-near-avenue-kleber-paris-7.jpg';
+import HotelKeppler23 from './Hotel-Keppler/room-near-avenue-kleber-paris.jpg';
+import HotelKeppler24 from './Hotel-Keppler/room-near-avenue-kleber-paris-2.jpg';
+
+// Hotel Dadou Images
+import HotelDadou1 from './Hotel-Dadou/bathroom-hotel-dadou-Paris.jpg';
+import HotelDadou2 from './Hotel-Dadou/chambre-hotel-dadou-Paris.jpg';
+import HotelDadou3 from './Hotel-Dadou/chamre-hotel-dadou-paris.jpg';
+import HotelDadou4 from './Hotel-Dadou/Dadou-hotel-paris-near-kleber.jpg';
+import HotelDadou5 from './Hotel-Dadou/Dadou-rooms-near-kleber-paris.jpg';
+import HotelDadou6 from './Hotel-Dadou/hotel-dadou-chambre.jpg';
+import HotelDadou7 from './Hotel-Dadou/hotel-dadou-meeting-room.jpg';
+import HotelDadou8 from './Hotel-Dadou/Hotel-Dadou-near-avenue-kleber-Paris.jpg';
+import HotelDadou9 from './Hotel-Dadou/hotel-dadou-paris=twin-room.jpg';
+import HotelDadou10 from './Hotel-Dadou/hotel-dadou-paris-chambre-twin.jpg';
+import HotelDadou11 from './Hotel-Dadou/Hotel-dadou-paris-near-avenue-kleber.jpg';
+import HotelDadou12 from './Hotel-Dadou/Hotel-dadou-paris-proche-kleber.jpg';
+import HotelDadou13 from './Hotel-Dadou/Hotel-Dadou-Pool.jpg';
+import HotelDadou14 from './Hotel-Dadou/Hotel-dadou-proche-avenue-kleber.jpg';
+import HotelDadou15 from './Hotel-Dadou/Hotel-Dadou-room.jpg';
+import HotelDadou16 from './Hotel-Dadou/Paris-hotel-dadou-proche-kleber.jpg';
+
+// Hotel Victor Hugo Kleber Images
+import HotelVictorHugoKleber1 from './Hotel-Victor-Hugo-Kleber/chambres-avenue-kleber-paris.jpg';
+import HotelVictorHugoKleber2 from './Hotel-Victor-Hugo-Kleber/Chambres-Hotel-Paris-proche-Kleber.jpg';
+import HotelVictorHugoKleber3 from './Hotel-Victor-Hugo-Kleber/Chambres-Hotel-proche-avenue-Kleber.jpg';
+import HotelVictorHugoKleber4 from './Hotel-Victor-Hugo-Kleber/chambres-proche-kleber-paris.jpg';
+import HotelVictorHugoKleber5 from './Hotel-Victor-Hugo-Kleber/Hotel-avenue-kleber-paris.jpg';
+import HotelVictorHugoKleber6 from './Hotel-Victor-Hugo-Kleber/hotel-near-avenue-kleber-paris.jpg';
+import HotelVictorHugoKleber7 from './Hotel-Victor-Hugo-Kleber/hotel-near-avenue-kleber-room.jpg';
+import HotelVictorHugoKleber8 from './Hotel-Victor-Hugo-Kleber/Hotel-Paris-Kleber.jpg';
+import HotelVictorHugoKleber9 from './Hotel-Victor-Hugo-Kleber/Hotel-paris-mear-Kleber.jpg';
+import HotelVictorHugoKleber10 from './Hotel-Victor-Hugo-Kleber/Hotel-proche-Kleber-paris.jpg';
+import HotelVictorHugoKleber11 from './Hotel-Victor-Hugo-Kleber/Paris-hotel-proche-Kleber.jpg';
+import HotelVictorHugoKleber12 from './Hotel-Victor-Hugo-Kleber/Paris-Kleber-quarter-Hotel.jpg';
+import HotelVictorHugoKleber13 from './Hotel-Victor-Hugo-Kleber/Rooms-Hotel-near-avenue-Kleber-Paris.jpg';
+import HotelVictorHugoKleber14 from './Hotel-Victor-Hugo-Kleber/Rooms-Hotel-near-Kleber-Paris.jpg';
+import HotelVictorHugoKleber15 from './Hotel-Victor-Hugo-Kleber/Rooms-near-avenue-kleber-Paris.jpg';
+
+// Export all images
+export {
+    MonsieurGeorges1, MonsieurGeorges2, MonsieurGeorges3, MonsieurGeorges4, MonsieurGeorges5,
+    MonsieurGeorges6, MonsieurGeorges7, MonsieurGeorges8, MonsieurGeorges9, MonsieurGeorges10,
+    MonsieurGeorges11, MonsieurGeorges12, MonsieurGeorges13,
+    LaDemeureMontaigne1, LaDemeureMontaigne2, LaDemeureMontaigne3, LaDemeureMontaigne4, LaDemeureMontaigne5,
+    LaDemeureMontaigne6, LaDemeureMontaigne7, LaDemeureMontaigne8, LaDemeureMontaigne9, LaDemeureMontaigne10,
+    LaDemeureMontaigne11, LaDemeureMontaigne12, LaDemeureMontaigne13, LaDemeureMontaigne14, LaDemeureMontaigne15,
+    LaDemeureMontaigne16, LaDemeureMontaigne17, LaDemeureMontaigne18, LaDemeureMontaigne19, LaDemeureMontaigne20,
+    LaDemeureMontaigne21, LaDemeureMontaigne22, LaDemeureMontaigne23, LaDemeureMontaigne24, LaDemeureMontaigne25,
+    LaDemeureMontaigne26,
+    HotelPastel1, HotelPastel2, HotelPastel3, HotelPastel4, HotelPastel5,
+    HotelPastel6, HotelPastel7, HotelPastel8, HotelPastel9, HotelPastel10,
+    HotelPastel11,
+    HotelPadam1, HotelPadam2, HotelPadam3, HotelPadam4, HotelPadam5,
+    HotelPadam6, HotelPadam7, HotelPadam8,
+    HotelKeppler1, HotelKeppler2, HotelKeppler3, HotelKeppler4, HotelKeppler5,
+    HotelKeppler6, HotelKeppler7, HotelKeppler8, HotelKeppler9, HotelKeppler10,
+    HotelKeppler11, HotelKeppler12, HotelKeppler13, HotelKeppler14, HotelKeppler15,
+    HotelKeppler16, HotelKeppler17, HotelKeppler18, HotelKeppler19, HotelKeppler20,
+    HotelKeppler21, HotelKeppler22, HotelKeppler23, HotelKeppler24,
+    HotelDadou1, HotelDadou2, HotelDadou3, HotelDadou4, HotelDadou5,
+    HotelDadou6, HotelDadou7, HotelDadou8, HotelDadou9, HotelDadou10,
+    HotelDadou11, HotelDadou12, HotelDadou13, HotelDadou14, HotelDadou15,
+    HotelDadou16,
+    HotelVictorHugoKleber1, HotelVictorHugoKleber2, HotelVictorHugoKleber3, HotelVictorHugoKleber4, HotelVictorHugoKleber5,
+    HotelVictorHugoKleber6, HotelVictorHugoKleber7, HotelVictorHugoKleber8, HotelVictorHugoKleber9, HotelVictorHugoKleber10,
+    HotelVictorHugoKleber11, HotelVictorHugoKleber12, HotelVictorHugoKleber13, HotelVictorHugoKleber14, HotelVictorHugoKleber15
+};
