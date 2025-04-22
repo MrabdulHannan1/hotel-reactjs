87530768d07fd06fbebcd0f3e67d136c50645fd8
abdul hannan <hannanmughal007@gmail.com>
2025-04-22 11:47:15 +0500

    err fix

--- src/components/contact-body.jsx
+++ src/components/contact-body.jsx
@@ -11,7 +11,7 @@ const ContactBody = () => {
       <section className='w-full h-100 md:h-120 lg:h-140 relative'>
         <img src={ContactImage} className='w-full h-full object-cover' alt='ContactImage-Image' />
         <div className='bg-black/40 absolute inset-0 justify-center items-center flex flex-col'>
-          <p className='text-white text-lg md:text-xl lg:text-2xl'>Company Name Here</p>
+          <p className='text-white text-lg md:text-xl lg:text-2xl'>Paris Hotel Kleber</p>
           <h1 className='text-4xl md:text-6xl lg:text-8xl font-bold text-white text-center tracking-[0] leading-tight text-shadow-lg/50'>
             CONTACT US
           </h1>
